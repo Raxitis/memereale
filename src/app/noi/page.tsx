b725b33d34cce57e5f801138df95ba0486cf61c1
Raxitis <raciti128@gmail.com>
2025-07-21 20:27:38 +0200

    Correzione del case dell'importazione del componente Silk

--- src/app/noi/page.tsx
+++ src/app/noi/page.tsx
@@ -1,4 +1,4 @@
-import Iridescence from "@/components/Iridescence";
+
 
 export default function Home() {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Silk from "@/components/silk";
+import Silk from "@/components/Silk";
 import Button from "@/components/button";
 import Link from "next/link";
 export default function Home() {
